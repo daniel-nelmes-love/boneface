08648a8e6947e8f92993e339f6429353eb957baa
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2017-03-10 21:19:58 +1300

    testing without mail()

--- contactUs/handler.php
+++ contactUs/handler.php
@@ -29,7 +29,7 @@ if( empty($errors))
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
 	$headers .= "X-Mailer: PHP/" . PHP_VERSION;
-	mail($to,$email_subject,$email_body,$headers);
+	// mail($to,$email_subject,$email_body,$headers);
 
 	header('Location: ../emailSent.html');
 	// echo '<script type="text/javascript">jsfunction();</script>';
